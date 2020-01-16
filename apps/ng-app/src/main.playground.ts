4e081dc524a6e3e955caec7198acb7b73f6ee5d4
Jason Lewis <jm.lewis@gmail.com>
2020-01-16 13:25:22 +1100

    Add Material modules and import into shared module. Add SASS styles dir with samples.

--- apps/ng-app/src/main.playground.ts
+++ apps/ng-app/src/main.playground.ts
@@ -1,11 +1,15 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PlaygroundModule } from 'angular-playground';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlaygroundModule } from 'angular-playground';
+import 'hammerjs';
+
+import { UiMatModule } from './app/shared/ui-mat.module';
 
 PlaygroundModule.configure({
   selector: 'app-root',
   overlay: false,
-  modules: [BrowserAnimationsModule]
+  modules: [BrowserAnimationsModule, FlexLayoutModule, UiMatModule],
 });
 
 platformBrowserDynamic().bootstrapModule(PlaygroundModule);
